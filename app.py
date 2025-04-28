b0f74f51b08df4725ba3b9cefd17b0ded3bebf48
MarwanIbni <109188374+GaeulTempest@users.noreply.github.com>
2025-04-28 16:57:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ with tabs[1]:  # Tab Game
                 st.error(f"❌ Gagal mengambil statistik: {e}")
 
             # Tombol Main Lagi
-       if st.button("🔄 Main Lagi")
+      if st.button("🔄 Main Lagi"):
     try:
         requests.post(f"{BASE_URL}/reset")
         st.success("✅ Game berhasil di-reset!")
@@ -223,5 +223,6 @@ with tabs[1]:  # Tab Game
 
     # Setelah reset, TIDAK rerun
     st.info("🚀 Silakan klik tombol Standby lagi untuk memulai permainan baru.")
+
 
                 
